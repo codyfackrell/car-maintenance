62204d98a1bd3c627d37a996cd0911d7a0dcc101
Cody Fackrell <codyfackrell56@gmail.com>
2025-10-29 22:43:18 -0600

    updated login page

---
+++ src/components/Login.tsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" required></input>
+    </form>
+  );
+}
+
+export default Login;
