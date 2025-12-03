0a483c8a8c3e31414ce315bb50e47ff64affdec6
Cody Fackrell <codyfackrell56@gmail.com>
2025-12-03 10:27:54 -0700

    added api endpoints and logic structure

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+import maintenanceRoutes from './routes/maintenanceRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use('/auth', authRoutes);
+app.use('/maintenance', maintenanceRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
