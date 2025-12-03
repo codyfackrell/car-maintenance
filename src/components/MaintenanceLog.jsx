c082edf7fd0126959bb54fde34b855f10dd0a8dd
Cody Fackrell <115325371+codyfackrell@users.noreply.github.com>
2025-12-03 07:56:21 -0700

    Rename Log to MaintenanceLog and modify layout
    Renamed Log component to MaintenanceLog and updated structure.

---
+++ src/components/MaintenanceLog.jsx
@@ -0,0 +1,14 @@
+function MaintenanceLog() {
+  return (
+    <table>
+      <tr>
+        <th>Date of Service</th>
+        <th>Service</th>
+        <th>Mileage</th>
+        <th>Notes</th>
+      </tr>
+    <table/>
+  );
+}
+
+export default MaintenanceLog;
