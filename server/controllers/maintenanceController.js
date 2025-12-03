1b4b1c6cd315612f696bdabde612eeb2e4580061
Cody Fackrell <codyfackrell56@gmail.com>
2025-12-03 14:10:00 -0700

    adding maintenance endpoints

--- server/controllers/maintenanceController.js
+++ server/controllers/maintenanceController.js
@@ -1,16 +1,32 @@
-export const getMaintenanceLog = (req, res => P
-
-)
-
-
-export const addMaintenanceItem = (req, res) => {
-
+export const getMaintenanceLog = async (req, res) => {
+    const { data, error } = await supabase
+    .from('maintenance_log')
+    .select()
 }
 
-export const editMaintenanceItem = (req, res) => {
 
+export const addMaintenanceItem = async (req, res) => {
+    // add item and returns the item
+    const { data, error } = await supabase
+    .from('maintenance_log')
+    .insert({ })
+    .select()
 }
 
-export const deleteMaintenanceItem = (req, res) => {
+export const editMaintenanceItem = async (req, res) => {
+    // update and returns the item
+    const { data, error } = await supabase
+    .from('maintenance_log')
+    .update({  })
+    .eq('id', 1)
+    .select()
+}
 
+export const deleteMaintenanceItem = async (req, res) => {
+    // delete and returns the item
+    const { data, error } = await supabase
+    .from('maintenance_log')
+    .delete()
+    .eq('id', 1)
+    .select()
 }
