6406fc267404c6116071ac135e34cb9f78aecb48
Cody Fackrell <codyfackrell56@gmail.com>
2025-12-04 10:34:36 -0700

    updated maintenance endpoint logic

--- server/controllers/maintenanceController.js
+++ server/controllers/maintenanceController.js
@@ -25,25 +25,58 @@ export const addMaintenanceItem = async (req, res) => {
     .from('maintenance_log')
     .insert({ date_of_service: serviceDate, service_item: serviceItem, mileage, notes })
     .select()
+    .eq('id', userId);
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+    return res.status(200).json(data);
+
     } catch (err) {
         return res.status(500).json({err: 'Server error'});
     }
 }
 
 export const editMaintenanceItem = async (req, res) => {
-    // update and returns the item
+    const updates = req.body;
+    try {
     const { data, error } = await supabase
     .from('maintenance_log')
-    .update({  })
-    .eq('id', 1)
+    .update(updates)
+    .eq('id', req.params.itemId)
     .select()
+    .eq('id', userId);
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    return res.status(200).json(data);
+
+    } catch (err) {
+        return res.status(500).json({err: 'Server error'});
+    }
+
 }
 
 export const deleteMaintenanceItem = async (req, res) => {
-    // delete and returns the item
+    const deleteItem = req.body;
+    try {
     const { data, error } = await supabase
     .from('maintenance_log')
-    .delete()
-    .eq('id', 1)
+    .delete(deleteItem)
+    .eq('id', req.params.itemId)
     .select()
+    .eq('id', userId);
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    return res.status(200).json(data);
+
+    } catch (err) {
+        return res.status(500).json({err: 'Server error'});
+    }
+
 }
