a77f468be571758416b276661124dbc4630b6f59
Cody Fackrell <codyfackrell56@gmail.com>
2025-12-10 22:32:39 -0700

    update delete confirm

--- src/components/DeleteConfirm.jsx
+++ src/components/DeleteConfirm.jsx
@@ -1,10 +1,11 @@
 function DeleteConfirm() {
-    return (
-        <></>
-    )
-
-};
+  return (
+    <>
+      <p>Are you sure you want to delete this item?</p>
+      <button>No</button>
+      <button>Yes</button>
+    </>
+  );
+}
 
 export default DeleteConfirm;
-    
-
